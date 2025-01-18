3071675cd263cc92b1d751da197612ac3486f44a
richard-acheampong <richardkaacheampong@gmail.com>
2025-01-18 21:20:45 +0000

    change output from print statement to return statement

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -9,10 +9,10 @@ logic including error handling, and main.py, which interfaces with the user thro
 def safe_divide(numerator, denominator):
     try: 
         result = float(numerator) / float(denominator)
-        return f"The result of the division is {result}."
+        return f"The result of the division is {result}"
     except ZeroDivisionError:
-        print("Error: Cannot divide by zero.")
+        return ("Error: Cannot divide by zero.")
     except ValueError:
-        print("Error: Please enter numeric values only.")
+        return ("Error: Please enter numeric values only.")
 
     
