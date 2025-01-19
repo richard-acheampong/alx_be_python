2b6d9db5d4823b46865fdc06855b805c67aba606
richard-acheampong <richardkaacheampong@gmail.com>
2025-01-19 12:57:57 +0000

    Modified test_add and test_subtrat to test_addition and test_subtraction

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,13 +7,13 @@ class TestSimpleCalculator(unittest.TestCase):
         """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(3, 2), 5)
         self.assertEqual(self.calc.add(-2, 3), 1)
         self.assertEqual(self.calc.add(-2, -2), -4)
 
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 3), 7)
         self.assertEqual(self.calc.subtract(-3, 2), -5)
         self.assertEqual(self.calc.subtract(10, -3), 13)
